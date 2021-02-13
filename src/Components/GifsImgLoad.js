f4797f7c87f283f7c2ee600b418ca536a1b7447c
Isaac <Isaacgrimaldo432@gmail.com>
2021-02-13 02:17:11 -0600

    se intregro todo para subir a github 2

--- src/Components/GifsImgLoad.js
+++ src/Components/GifsImgLoad.js
@@ -4,7 +4,7 @@ const GifsImgLoad = ( {id , gif , title}) => {
     return (
         <div className = 'gif-cpntent animate__animated animate__fadeInUp animate__fast ' >
              <img src ={gif} alt ={title} className ="animate__animated animate__backInDown"/>
-             <p className="animate__animated animate__fadeInUp animate__slow	2s">{title}</p>  
+             <p className="animate__animated animate__fadeInDownBig animate__slow">{title}</p>  
         </div>
     )
  }
